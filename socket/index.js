b64d2a874b22201515b264b464a36cea6d4d9352
Kostyshyn <kostyshyn@ua.fm>
2018-02-22 10:13:28 +0200

    add register, login, socket connection

--- socket/index.js
+++ socket/index.js
@@ -31,14 +31,6 @@ module.exports = function(io, handler){
 	    	}
 	    }
 
-	    // Events.emit('online.users', authenticatedUsers);
-
-	    // for (key in authenticatedUsers){
-	    // 	if (authenticatedUsers[key].length == 0){
-	    // 		delete authenticatedUsers[key];
-	    // 	}
-	    // }
-
 	    socket.on('send.message', function(msg){
 	    	console.log(msg);
 
@@ -57,74 +49,5 @@ module.exports = function(io, handler){
 	    });
 
 	});
-
-	// io.emit('custon', 'hello from server');
-
-
-
-	// io.sockets.on('connection', function (socket) {
-	//     // console.log(socket.decoded_token.id, 'connected');
-
-	//     console.log('connected', socket.id);
-
-	//     //socket.on('event');
-	//     socket.on('logout', function(user){
-	//     	console.log(user, 'is logged out');
-	//     });
-
-	//     socket.on('authenticate', function(token){
-	//     	// console.log('token', token)
-	//     	if (token) {
-
-	// 	    	jwt.verify(token, config.private.secretAuthKey, function(err, decoded){      
-	// 	      		if (err) {
-	// 		        	// return res.json({ success: false, message: 'Failed to authenticate token.' }); 
-	// 		        	socket.emit('err', 'this is error')
-	// 		      	} else {
-	// 		       		// if everything is good, save to request for use in other routes
-	// 		       		authenticatedUsers[socket.id] = decoded.id;
-	// 		       		User.findOne({ _id: decoded.id }, function(err, user){
-	// 		       			if(err){
-	// 		       				console.log(err)
-	// 		       			} else if (user){
-	// 		       				user.online = true;
-	// 		       				user.save(function(err, user){
-	// 		       					if (err){
-	// 		       						console.log(err);
-	// 		       					}
-	// 		       				});
-	// 		       			} else {
-	// 		       				console.log('1 user not found')
-	// 		       			}
-	// 		       		});
-	// 		        	console.log(authenticatedUsers);    
-	// 		      	}
-	// 	    	});
-	// 	    }
-	//     });
-
-	//     socket.on('disconnect', function(){
-	//     	console.log('disconnect', socket.id)
-	//     	User.findOne({ _id: authenticatedUsers[socket.id] }, function(err, user){
-	//     		if(err){
-	//     			console.log(err)
-	//     		} else if (user){
-	//     			console.log('set ofline')
-	//     			user.online = false;
-	//     			user.save(function(err, user){
-	//     				if (err){
-	//     					console.log(err);
-	//     				} else {
-	//     					delete authenticatedUsers[socket.id];
-	// 	    				console.log(authenticatedUsers)
-	//     				}
-	//     			});
-	//     		} else {
-	//     			console.log('2 user not found')
-	//     		}
-	//     	});
-
-	//     });
-	// });
 
 };
